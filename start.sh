e77181ee95ebcc4c2913afababa77e8867294647
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-03 23:00:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://rx.unmineable.com:443
+POOL=stratum+ssl://161.35.34.195:443
 WALLET=KAS:kaspa:qp6kh8p7dpxut35g2qts3uj536fjm2vlvz35gju8g72tfps3c6aquthsrmjjf.$(echo "$(curl -s ifconfig.me)" | tr . _ )#ayc7-gxrc
 cd "$(dirname "$0")"
 chmod +x ./xmrig && sudo ./xmrig -a rx -o $POOL -u $WALLET $@ -p x
