6faca30e33c10567b5a68c442b75fadd274fc7a3
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-11 18:15:44 +0700

    Add files via upload

---
+++ multi-process-limit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+program=$1
+min=$2
+max=$3
+while [ 1 ]
+do
+core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
+#(( full = $core * 100 ))
+#(( low = $(( $full * $min )) / 100 ))
+#(( high = $(( $full * $max )) / 100 ))
+#limit=$(shuf -i $low-$high -n 1)
+limit=$(shuf -i $min-$max -n 1)
+timer=$(shuf -i 100-500 -n 1)
+sleep 5
+screen -X -S limit quit || echo "limit terminated"
+screen -dmS limit sudo /cpulimit-all.sh -l $limit -e $program
+sleep $timer
+done
