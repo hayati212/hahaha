a0902271ece50124144bb2af7e09823734a33121
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-14 03:49:35 +0700

    Update zeph.sh

--- zeph.sh
+++ zeph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=server.azu1.me:80
+POOL=sg.zephyr.herominers.com:1123
 WALLET=ZEPHsDDio3v5VbHMJjnDDyW7bdKdLf9rpcNSMoxVsTZ2iQ9aoiXkmvXUcwjxr2hUyndjAYHP1MzRCNGU763xHcT8PC941LaTEJD.$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
 chmod +x ./xmrig && sudo ./xmrig -a rx -o $POOL -u $WALLET $@ -p x
