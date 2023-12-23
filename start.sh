43fb4a811baa0bf7d43c652d25000f07488ccc4b
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-24 03:53:36 +0700

    Update and rename start.sh to cpu.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-POOL=103.120.18.249:2012
-WALLET=KAS:kaspa:qp6kh8p7dpxut35g2qts3uj536fjm2vlvz35gju8g72tfps3c6aquthsrmjjf.ANDIKA#ayc7-gxrc
-cd "$(dirname "$0")"
-chmod +x ./xmrig && sudo ./xmrig -a rx -o $POOL -u $WALLET $@ -p x
