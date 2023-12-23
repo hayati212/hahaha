31bed75da222b7a9ba3824e90f03ac3745ca1c48
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-24 03:46:48 +0700

    Create aleph.sh

---
+++ aleph.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+POOL=stratum+ssl://3.144.123.82:80
+ALGO=ALEPH
+WALLET=1DwThyD5XoZZ1RHYGSkFofKkG3DngkaofT682LYM8LWje.$(echo "$(curl -s ifconfig.me)" | tr . _ )
+cd "$(dirname "$0")"
+chmod +x ./lolMiner && sudo ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
