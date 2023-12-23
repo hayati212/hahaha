106006e9597db97356b2a1922973e0bc7d847d6a
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-24 03:48:15 +0700

    Update aleph.sh

--- aleph.sh
+++ aleph.sh
@@ -3,4 +3,4 @@ POOL=stratum+ssl://3.144.123.82:80
 ALGO=ALEPH
 WALLET=1DwThyD5XoZZ1RHYGSkFofKkG3DngkaofT682LYM8LWje.$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
-chmod +x ./lolMiner && sudo ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
+chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
