d75602448b07b06154d95308143f997ed9552d30
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-24 03:46:12 +0700

    Delete aleph.sh

--- aleph.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-POOL=stratum+ssl://3.144.123.82:80
-ALGO=ALEPH
-WALLET=1DwThyD5XoZZ1RHYGSkFofKkG3DngkaofT682LYM8LWje
-cd "$(dirname "$0")"
-chmod +x ./lolMiner && sudo ./lolMiner --algo $ALGO --pool $POOL --user $WALLET.$(echo "$(curl -s ifconfig.me)" | tr . _ ) $@
