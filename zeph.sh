b6e82eccd4884630f1d4b7d91523ec3c0a1eed57
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-24 21:17:44 +0700

    Delete zeph.sh

--- zeph.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-POOL=sg.zephyr.herominers.com:1123
-WALLET=ZEPHsDDio3v5VbHMJjnDDyW7bdKdLf9rpcNSMoxVsTZ2iQ9aoiXkmvXUcwjxr2hUyndjAYHP1MzRCNGU763xHcT8PC941LaTEJD.$(echo "$(curl -s ifconfig.me)" | tr . _ )
-cd "$(dirname "$0")"
-chmod +x ./xmrig && sudo ./xmrig -a rx -o $POOL -u $WALLET $@ -p x
