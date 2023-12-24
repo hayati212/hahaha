859ff9efa8661b63378a758b98971a57de6392b2
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-25 02:15:09 +0700

    Update avax.sh

--- avax.sh
+++ avax.sh
@@ -2,6 +2,6 @@
 POOL=stratum+ssl://3.144.123.82:443
 ALGO=ALEPH
 WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.$IP
-IP=$(echo "$(curl -s ifconfig.me)" | tr . _ )#tw4q-eldi
+IP=(echo "$(curl -s ifconfig.me)" | tr . _ )#tw4q-eldi
 cd "$(dirname "$0")"
 chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
