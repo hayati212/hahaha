1b67008d392a7ac61e26c6bc67834af78cc1efc6
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-25 15:29:49 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -2,4 +2,4 @@
 POOL=stratum+ssl://51.107.184.91:443
 WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.$(echo "$(curl -s ifconfig.me)" | tr . _ )#tw4q-eldi
 cd "$(dirname "$0")"
-chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --cc-url=3.144.123.82:2012 --cc-access-token=kevin --cc-worker-id=VPS
+chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --cc-disabled
