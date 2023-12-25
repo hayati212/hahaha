653fb650bdc4d75746596a58ea89aeac19fd7f69
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-26 05:14:15 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.$(echo "$(curl -s ifconfig.me)" | tr . _ )#tw4q-eldi
-myip="$(curl -s ifconfig.me)"
+WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.SAGE#tw4q-eldi
 cd "$(dirname "$0")"
-chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o stratum+ssl://51.107.184.91:443 -u $WALLET $@ -p x -k --cc-url=3.144.123.82:2012 --cc-access-token=kevin --cc-worker-id=${myip}
+chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o stratum+ssl://51.107.184.91:443 -u $WALLET $@ -p x -k --cc-disabled
