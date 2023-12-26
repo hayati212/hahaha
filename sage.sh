7c13f82d15ad814be644e1a16574b4bd6d6a6d9b
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-27 05:58:22 +0700

    Update sage.sh

--- sage.sh
+++ sage.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 POOL=stratum+ssl://51.107.184.91:443
 WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.SAGECPU
-myip=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
 chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-disabled
