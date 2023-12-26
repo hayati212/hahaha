383bca8eeb76506b4fb7e0969cc4c0f14cf7c1a7
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-27 05:47:04 +0700

    Update avax.sh

--- avax.sh
+++ avax.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://3.144.123.82:443
+POOL=stratum+ssl://51.107.184.91:80
 ALGO=ALEPH
 WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.SAGEGPU#tw4q-eldi
 cd "$(dirname "$0")"
