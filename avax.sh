bba51697987d799bb7e4ff14dd3f1ff58136926a
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-26 23:04:14 +0700

    Update avax.sh

--- avax.sh
+++ avax.sh
@@ -4,4 +4,4 @@ ALGO=ALEPH
 myip="$(curl -s ifconfig.me) | tr . _"
 WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.${myip}#tw4q-eldi
 cd "$(dirname "$0")"
-chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET.${myip} $@
+chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
