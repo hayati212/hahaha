3fdd06d13077048452e415a6d57dd250a4f01a28
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 20:54:57 +0700

    Update iron.sh

--- iron.sh
+++ iron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://20.124.247.146:80
+POOL=stratum+ssl://20.124.247.146:443
 ALGO=IRONFISH
 WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71+20491097150.ANDIKA$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
