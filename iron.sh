bff5563e8d7169ba85f13908305035ae3d34382d
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 21:26:34 +0700

    Update iron.sh

--- iron.sh
+++ iron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+tcp://20.124.247.146:80
+POOL=stratum+tcp://51.107.184.91:80
 ALGO=IRONFISH
 WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.GPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 cd "$(dirname "$0")"
