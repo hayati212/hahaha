6fc102f4d7c8e342d75548c7dec66a42b77d08cf
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 21:15:02 +0700

    Update iron.sh

--- iron.sh
+++ iron.sh
@@ -6,8 +6,8 @@
 #chmod +x ./bzminer && clear && ./bzminer -a $ALGO -p $POOL -w $WALLET $@
 
 #!/bin/bash
-POOL=20.124.247.146:443
-ALGO=IRONFISH
+POOL=proxy+tcp://20.124.247.146:443
+ALGO=ironfish
 WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.GPU$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 cd "$(dirname "$0")"
-chmod +x ./bzminer && clear && ./bzminer -a $ALGO -p $POOL -w $WALLET $@
+chmod +x ./miner && clear && ./miner -a $ALGO -s $POOL -u $WALLET $@
