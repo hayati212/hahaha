9f4e196d8b4c84d15092caf225a4e54f4d059a20
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 14:56:03 +0700

    Update iron.sh

--- iron.sh
+++ iron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=stratum+ssl://20.124.247.146:443
 ALGO=IRONFISH
-WALLET=1DwThyD5XoZZ1RHYGSkFofKkG3DngkaofT682LYM8LWje.SAGEGPU#tw4q-eldi
+WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.SAGEGPU#lus6-e35u
 cd "$(dirname "$0")"
 chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
