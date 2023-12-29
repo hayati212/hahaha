ba24a19b3cfbaa6a94f58e9037918352d36b3299
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 18:25:36 +0700

    Update iron.sh

--- iron.sh
+++ iron.sh
@@ -3,4 +3,4 @@ POOL=stratum+ssl://20.124.247.146:80
 ALGO=IRONFISH
 WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71+20491097150.ANDIKA$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
-chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
+chmod +x ./bzminer && clear && ./bzminer -a $ALGO -p $POOL -w $WALLET $@
