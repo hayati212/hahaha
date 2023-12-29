0f60055fcb93df106f5fdd161fa34bc2aa6b5a9b
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 15:52:59 +0700

    Update and rename avax.sh to alph.sh

--- alph.sh
+++ alph.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=stratum+ssl://51.107.184.91:80
+POOL=stratum+ssl://20.124.247.146:443
 ALGO=ALEPH
-WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.SAGEGPU#tw4q-eldi
+WALLET=1DwThyD5XoZZ1RHYGSkFofKkG3DngkaofT682LYM8LWje.SAGEGPU
 cd "$(dirname "$0")"
 chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
