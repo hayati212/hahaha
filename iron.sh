d088e1da0ae31e4a03a7144cfaa418eb0d0ec0d8
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2023-12-29 21:20:28 +0700

    Update iron.sh

--- iron.sh
+++ iron.sh
@@ -10,4 +10,4 @@ POOL=stratum+tcp://20.124.247.146:80
 ALGO=ironfish
 WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.GPU$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 cd "$(dirname "$0")"
-chmod +x ./miner && clear && ./miner -a $ALGO -o $POOL -u $WALLET $@
+chmod +x ./rigel && clear && ./rigel -a $ALGO -o $POOL -u $WALLET $@
