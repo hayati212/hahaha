00ea3fcf0c9873168c55a2f328ffa576ac2e336c
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-01 17:53:58 +0700

    Update sage.sh

--- sage.sh
+++ sage.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://20.124.247.146:80
-WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.ANDIKA$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
-myip=CPU$(echo "$(curl -s ifconfig.me)" | tr . _ )
+POOL=stratum+ssl://172.200.229.146:80
+WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.CPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 cd "$(dirname "$0")"
 chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-disabled
