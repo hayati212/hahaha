ebda4a8cf9dfb618e6d475638354308082c3d993
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-01 15:22:48 +0700

    Update iron.sh

--- iron.sh
+++ iron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=51.107.184.91:80
 ALGO=IRONFISH
-WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.GPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
+WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.GPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 cd "$(dirname "$0")"
 chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
