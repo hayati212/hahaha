3ad35051d067fc5573671126440b3d96e0c3b2ce
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-01 17:51:55 +0700

    Delete alph.sh

--- alph.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-POOL=stratum+ssl://20.124.247.146:443
-ALGO=ALEPH
-WALLET=solo:1DwThyD5XoZZ1RHYGSkFofKkG3DngkaofT682LYM8LWje.SAGEGPU
-cd "$(dirname "$0")"
-chmod +x ./lolMiner && clear && ./lolMiner --algo $ALGO --pool $POOL --user $WALLET $@
